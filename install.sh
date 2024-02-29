60ed428b067385c8b8f350ff9b29563f42544213
Venelin <vdechkov@visteon.com>
2024-02-29 12:02:42 +0200

    install prefix

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 set -e
 
+# Install location
+PREFIX=/usr/local
+
 # Compile lib
 options="-std=c++17 -Ofast -flto -march=native -DNDEBUG -Wall -Wextra -Wno-parentheses -Wno-unused-result -s -pipe"
 g++ -c $options commands.cpp childProcess.cpp
 
 # Create lib
 ar r libcppipe.a commands.o childProcess.o
-mv libcppipe.a /usr/local/lib
+mv libcppipe.a ${PREFIX}/lib
 rm commands.o childProcess.o
 
 # Compile cppipe
 g++ -o cppipe $options cppipe.cpp -lcppipe
 chmod 755 cppipe
-mv cppipe /usr/local/bin
+mv cppipe ${PREFIX}/bin
 
-# Add headers
-mkdir -p /usr/local/include/cppipe
-cp basicTypes.h commands.hpp childProcess.hpp /usr/local/include/cppipe
+# Install headers
+mkdir -p ${PREFIX}/include/cppipe
+cp basicTypes.h commands.hpp childProcess.hpp ${PREFIX}/include/cppipe
